6301304d40ef89c3654c29e41c4b9f519445d74b
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-11-05 14:01:44 +0300

    LR_8_TASK_2 READY

--- LR_8_TASK_2/LR_8_TASK_2/LR_8_TASK_2.cpp
+++ LR_8_TASK_2/LR_8_TASK_2/LR_8_TASK_2.cpp
@@ -1,98 +1,239 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+#include <set>
+#include <string.h>
+#include <string>
 
-/*
-В модели безопасности "Chinese Wall" система считается безопасной, если соблюдены следующие два условия:
-1) субъект может читать объекты только в компании А либо в другой компании, которая не в конфликте с компанией А
-2) субъект может писать в объекты только в компании А
-*/
 using namespace std;
 
-struct Object {
-	string ObjName;
-	string ObjFirmName;
-	string HistoryOfOpers;
-};
+// фирма - набор объектов
+vector<set<int>> firms;
+// конфликт интересов - набор фирм
+vector<set<int>> conflicts;
+// история запросов для субъектов - список из последовательностей запросов каждого субъекта
+// запрос - индекс объекта + тип запроса
+vector<vector<pair<int, string>>> accessSubjectHistory;
+// история запросов для объектов - список из последовательностей запросов каждого объекта
+// запрос - индекс субъекта + тип запроса
+vector<vector<pair<int, string>>> accessObjectHistory;
 
-
-struct Firm {
-	string FirmName;
-	vector <Object> Objects = vector <Object>();
-	vector <string> ConflictsName = vector <string>();
-};
-
-int main()
-{
-	setlocale(LC_ALL, "RUS");
-	int F, O, S; // фирмы, объекты, субъекты, а точнее - их количество
-
-	vector <Firm> Firms = vector <Firm>();
-	vector <Object> Objects = vector <Object>();
-	cout << "Введите кол-во фирм" << endl;
-	cin >> F;
-
-	int i = 0;
-	while (i != F)
-	{
-		Firm tempFirm;
-		Object tempObj;
-		string FirmName;
-		cout << "Введите имя фирмы" << endl;
-		cin >> FirmName;
-		tempFirm.FirmName = FirmName;
-		tempObj.ObjFirmName = FirmName;
-		int k;
-		cout << "Введите кол-во объектов в фирме "  << FirmName << endl;
-		cin >> k;
-		while (k != 0)
-		{
-			string ObjName;
-			cout << "Введите имя объекта" << endl;
-			cin >> ObjName;
-			tempObj.ObjName = ObjName;
-
-			tempFirm.Objects.push_back(tempObj);
-			Objects.push_back(tempObj);
-			k--;
-		}
-		Firms.push_back(tempFirm);
-		i++;
-	}
-
-	cout << "Мы не ввели конфликты фирм, введём" << endl;
-	cout << "Конфликтных фирм для одной фирмы может быть всего " << Firms.size() - 1 << endl;
-
-	i = 0;
-	while (i != F)
-	{
-		int k;
-		cout << "Введите кол-во конфликтных фирм в фирме " << Firms[i].FirmName << endl;
-		cin >> k;
-		if (k > Firms.size() - 1)
-		{
-			k = Firms.size() - 1;
-		}
-		while (k != 0)
-		{
-			string tempConfl;
-			cout << "Введите конфликтную фирму" << endl;
-			cin >> tempConfl;
-			Firms[i].ConflictsName.push_back(tempConfl);
-			k--;
-			/*for (int d = 0; d < Firms.size(); d++)
-			{
-				if (Firms[d].FirmName == tempConfl)
-				{
-					Firms[d].ConflictsName.push_back(Firms[i].FirmName);
-				}
-			}*/
-		}
-		i++;
-	}
-
-
-
+bool contains(set<int>& s, int elt) {
+    return (find(s.begin(), s.end(), elt) != s.end());
 }
 
+vector<string> split(string line) {
+    vector<string> result = vector<string>();
 
+    for (int i = 0; i < line.size(); i++) {
+        if (line[i] == ' ') {
+            result.push_back(line.substr(0, i));
+            vector<string> tail = split(line.substr(i + 1, line.size() - i - 1));
+            result.insert(result.end(), tail.begin(), tail.end());
+            break;
+        }
+    }
+
+    if (result.size() == 0) {
+        result.insert(result.begin(), line);
+    }
+
+    return result;
+}
+
+int getFirm(int forObject) {
+    for (int i = 0; i < firms.size(); i++) {
+        if (contains(firms[i], forObject)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int getInterestGroup(int forObject) {
+    int firm = getFirm(forObject);
+
+    for (int i = 0; i < conflicts.size(); i++) {
+        if (contains(conflicts[i], firm)) {
+            return i;
+        }
+    }
+
+    return -1;
+};
+
+void printFirmObjects(int firmIndex) {
+    for (set<int>::iterator i = firms[firmIndex].begin(); i != firms[firmIndex].end(); i++) {
+        cout << *i << ", ";
+        i++;
+    }
+    cout << endl;
+}
+
+void printReportForSubject(int subjectIndex) {
+    for (int i = 0; i < accessSubjectHistory[subjectIndex].size(); i++) {
+        cout << "access " << accessSubjectHistory[subjectIndex][i].second << " with object " << accessSubjectHistory[subjectIndex][i].first << " in firm " << getFirm(accessSubjectHistory[subjectIndex][i].first);
+    }
+}
+
+void printReportForObject(int objectIndex) {
+    for (int i = 0; i < accessObjectHistory[objectIndex].size(); i++) {
+        cout << "access " << accessObjectHistory[objectIndex][i].second << " with object " << accessObjectHistory[objectIndex][i].first << " in firm " << getFirm(accessObjectHistory[objectIndex][i].first);
+    }
+}
+
+bool canRead(int subjectIndex, int objectIndex) {
+    int interestGroup = getInterestGroup(objectIndex);
+    int objectFirm = getFirm(objectIndex);
+
+    set<int> accessGroups = set<int>();
+    set<int> firmGroups = set<int>();
+
+    for (int i = 0; i < accessSubjectHistory[subjectIndex].size(); i++) {
+        accessGroups.insert(getInterestGroup(accessSubjectHistory[subjectIndex][i].first));
+        firmGroups.insert(getFirm(accessSubjectHistory[subjectIndex][i].first));
+    }
+
+    return !contains(accessGroups, interestGroup) || contains(firmGroups, objectFirm);
+}
+
+bool canWrite(int subjectIndex, int objectIndex) {
+    int objectFirm = getFirm(objectIndex);
+    int objectInterest = getInterestGroup(objectIndex);
+    bool isReadFromAnoutherFirm = false;
+
+    set<int> readedFirms = set<int>();
+
+    for (int i = 0; i < accessSubjectHistory[subjectIndex].size(); i++) {
+        if (accessSubjectHistory[subjectIndex][i].second == "r" &&
+            getFirm(accessSubjectHistory[subjectIndex][i].first) != objectFirm &&
+            getInterestGroup(accessSubjectHistory[subjectIndex][i].first) == objectInterest) {
+            isReadFromAnoutherFirm = true;
+            break;
+        }
+    }
+
+    return canRead(subjectIndex, objectIndex) && !isReadFromAnoutherFirm;
+}
+
+void read(int subjectIndex, int objectIndex) {
+    if (canRead(subjectIndex, objectIndex)) {
+        accessSubjectHistory[subjectIndex].push_back(pair<int, string>(objectIndex, "r"));
+        accessObjectHistory[objectIndex].push_back(pair<int, string>(subjectIndex, "r"));
+        cout << "accepted" << endl;
+    }
+    else {
+        cout << "refused" << endl;
+    }
+}
+
+void write(int subjectIndex, int objectIndex) {
+    if (canWrite(subjectIndex, objectIndex)) {
+        accessSubjectHistory[subjectIndex].push_back(pair<int, string>(objectIndex, "w"));
+        accessObjectHistory[objectIndex].push_back(pair<int, string>(subjectIndex, "w"));
+        cout << "accepted" << endl;
+    }
+    else {
+        cout << "refused" << endl;
+    }
+}
+
+void setUpChinaWall(int objCount, int subjCount, int firmCount, int interCount) {
+    firms = vector<set<int>>(firmCount, set<int>());
+    conflicts = vector<set<int>>(interCount, set<int>());
+
+    accessSubjectHistory = vector<vector<pair<int, string>>>(subjCount);
+    accessObjectHistory = vector<vector<pair<int, string>>>(objCount);
+}
+
+void doCommand(string command) {
+    vector<string> words = split(command);
+
+    if (words[0] == "start") {
+        accessSubjectHistory = vector<vector<pair<int, string>>>(accessSubjectHistory.size());
+        accessObjectHistory = vector<vector<pair<int, string>>>(accessObjectHistory.size());
+    }
+    else if (words[0] == "read") {
+        int subjectIndex = stoi(words[1]);
+        int objectIndex = stoi(words[2]);
+        read(subjectIndex, objectIndex);
+    }
+    else if (words[0] == "write") {
+        int subjectIndex = stoi(words[1]);
+        int objectIndex = stoi(words[2]);
+        write(subjectIndex, objectIndex);
+    }
+    else if (words[0] == "report_subject") {
+        int subjectIndex = stoi(words[1]);
+        printReportForSubject(subjectIndex);
+    }
+    else if (words[0] == "report_object") {
+        int objectIndex = stoi(words[1]);
+        printReportForObject(objectIndex);
+    }
+    else if (words[0] == "brief_case") {
+        int firmIndex = stoi(words[1]);
+        printFirmObjects(firmIndex);
+    }
+    else {
+        cout << "invalid command" << endl;
+    }
+
+    cout << endl;
+}
+
+int main() {
+    //0 - 0 
+    //1 - 1, 2, 3
+    //2 - 4, 5 
+    // 
+    //0 - 0, 1
+    //1 - 2
+
+    setUpChinaWall(6, 1, 3, 2);
+
+    firms[0].insert(0);
+    firms[1].insert(1);
+    firms[1].insert(2);
+
+    firms[1].insert(3);
+    firms[2].insert(4);
+
+    firms[2].insert(5);
+
+    conflicts[0].insert(0);
+    conflicts[0].insert(1);
+
+    conflicts[1].insert(2);
+
+    //// reading test
+    //cout << "test 1:" << endl;
+    //"read 0 0" // ok
+    //"read 0 3" // ошибка - объект в другой фирме и в том же конфликте интересов
+    //"read 0 5" // ок - объект из другого конфликта интересов
+
+    //cout << "test 2:" << endl;
+    //"start"
+    //"read 0 3" // ок
+    //"read 0 4" // ок - тот же конфликт интересов, но та же фирма
+
+    //cout << "test 3:" << endl;
+    //"start"
+    //"write 0 0" // ок
+    //"write 0 1" // ок - объект в той же фирме
+    //"write 0 3" // ошибка - объект в другой фирме и в том же конфликте интересов
+    //"write 0 5" // ок - объект в другой фирме, но в другом конфликте интересов
+
+    while (true) {
+        string s;
+        cin >> s;
+        int i, j;
+        cin >> i >> j;
+        if (s == "read") {
+            doCommand("read " + to_string(i) + ' ' + to_string(j));
+        }
+        else {
+            doCommand("write " + to_string(i) + ' ' + to_string(j));
+        }
+    }
+    return 0;
+}
